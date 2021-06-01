9c8aa4d87fa1dd9dcdc78390bc632efeecf04773
Jaden Arceneaux <arceneauxjaden@gmail.com>
2021-06-01 15:58:44 -0700

    note test data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
 import NewNoteForm from "./components/NewNoteForm";
+import Note from "./components/Note"
 
 function App() {
   return (
     <div className="App">
       <h1>Pop Notes</h1>
       <NewNoteForm/>
+      <hr />
+      <div className="notes">
+        <Note content="Hello" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+        <Note content="Hello2" link="https://google.com/"/>
+      </div>
     </div>
   );
 }
